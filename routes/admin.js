8cefc2763b49648c8a921b573305dd8c5137d5d0
PhongNDH <phongndhdev@gmail.com>
2024-06-17 21:13:23 +0700

    update api

--- routes/admin.js
+++ routes/admin.js
@@ -6,11 +6,12 @@ import {
   deleteService,
 } from "../controllers/admin.js";
 
+
 const router = express.Router();
 
 router.post("/create-service", createService);
 router.get("/get-service", getService);
 router.put("/update-service/:serviceId", updateService);
-router.delete("delete-service/:serviceId", deleteService);
+router.delete("/delete-service/:serviceId", deleteService);
 
 export { router };
